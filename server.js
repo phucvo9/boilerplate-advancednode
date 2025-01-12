0012164435fb6b45ef86d23c298bc093266ed8c1
Phuc Vo <vhphuc.it@gmail.com>
2025-01-12 05:44:30 +0000

    pug

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+app.set('view engine', 'pug');
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
