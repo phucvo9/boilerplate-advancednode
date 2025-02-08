68ee2e5f1aa65a74570f39b694d5882069394dee
Phuc Vo <vhphuc.it@gmail.com>
2025-02-08 07:35:19 +0000

    3

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-fccTesting(app); //For FCC testing purposes
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,5 +31,5 @@ app.route('/').get((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
